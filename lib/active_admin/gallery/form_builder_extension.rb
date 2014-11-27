469ffc7e45576839e9bcd3d3b01c9ed5dda220d3
Stefano Pau <stefano.pau@welaika.com>
2014-11-27 16:05:32 +0100

    Update form_builder_extension.rb

--- lib/active_admin/gallery/form_builder_extension.rb
+++ lib/active_admin/gallery/form_builder_extension.rb
@@ -24,7 +24,7 @@ module ActiveAdmin
             template.link_to("Edit", "#")
           end
 
-          fields = with_new_form_buffer do
+          fields = input_wrapping do
             template.content_tag(:li, class: "fields") do
               template.content_tag(:ol) do
                 i.input :image, as: :dragonfly, input_html: options
